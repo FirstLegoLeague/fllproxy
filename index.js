c2143fa0393bc7789ebb69cd3fa6ba9104c7b015
Rikkert Koppes <rikkert@rikkertkoppes.com>
2015-10-28 20:42:22 +0100

    added spawning of services

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var proxyMiddleware = require('http-proxy-middleware');
 var forever = require('forever-monitor'); 
 var config = require('./config.json');
+var path = require('path');
+var appPath = config.appPath;
 
+var children = spawnProcesses(config.contexts);
 var proxies = createProxies(config.contexts);
+
+function spawnProcesses(config) {
+    Object.keys(config).map(function(context) {
+        var port = config[context].port;
+        var auth = config[context].auth;
+        var child = new (forever.Monitor)(appPath, {
+            max: 3,
+            silent: false,
+            args: ['-p',port,'-d',path.join('data',context)],
+            cwd: path.dirname(appPath),
+        });
+
+        child.on('exit', function () {
+            console.log('child for region',context,'has exited after 3 restarts. Port',port);
+        });
+
+        child.start();
+        return child;
+    });
+}
 
 function createProxies(config) {
     return Object.keys(config).map(function(context) {
@@ -18,7 +41,7 @@ function createProxies(config) {
     });
 }
 
-var app = express();
+var app = express(); 
     //create all proxies;
     proxies.forEach(function(proxy) {
         app.use(proxy);
