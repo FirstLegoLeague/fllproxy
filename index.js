9ebf8100925452875bd475010238a366a260d358
Martin Poelstra <martin@beryllium.net>
2015-11-11 23:03:51 +0100

    Add comments on shutdown behaviour, fix whitespace.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var proxyMiddleware = require('http-proxy-middleware');
-var forever = require('forever-monitor'); 
+var forever = require('forever-monitor');
 var config = require('./config.json');
 var path = require('path');
 var appPath = config.appPath;
@@ -47,7 +47,7 @@ function createProxies(config) {
     });
 }
 
-var app = express(); 
+var app = express();
     app.get('/:context$',function(req,res) {
         res.redirect('/'+req.params.context+'/');
     });
@@ -63,16 +63,20 @@ var app = express();
 //exit handling, from http://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
 function exitHandler(options, err) {
     console.log(options.sig);
+    // Note: children are already started with "-s" option, so they will already
+    // shut down when they detect their stdin is closed.
     children.forEach(function(child) {
-	if (child.running) {
-	        child.kill(true);
-	}
+        if (child.running) {
+            child.kill(true);
+        }
     });
     if (err) console.log(err.stack);
     if (options && options.exit) {
-	setTimeout(function() {
-		process.exit();
-	},100);
+        // Slight delay to allow signals to be sent, before this process exits.
+        // Again, no big deal if this fails.
+        setTimeout(function() {
+            process.exit();
+        },100);
     }
 }
 
